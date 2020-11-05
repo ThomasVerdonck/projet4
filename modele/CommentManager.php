ca1b958f7a50fdfd47a2bc597a22d7ed7953cd49
Thomas Verdonck <thomas_verdonck@yahoo.fr>
2020-11-05 11:54:31 +0100

    modele istancié

---
+++ modele/CommentManager.php
@@ -0,0 +1,53 @@
+<?php
+require_once("Manager.php");
+class CommentManager extends Manager
+{
+    public function getComments($postId)//récupère les commentaires associés à un ID de post
+    {
+        $bdd = $this->getBdd();
+        $comments = $bdd->prepare('SELECT id, id_billet, auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire_fr, signalements FROM commentaires WHERE id_billet = ? ORDER BY date_commentaire DESC');
+        $comments->execute(array($postId));
+        return $comments;
+    }
+
+    // Insertion des variables à l'aide d'une requête préparée
+    // On récupère les variables à partir de $_GET['id'] $_POST['author'] et $_POST['comment']
+    public function postComment($postId, $author, $comment){
+        $bdd = $this->getBdd();
+        $comments = $bdd->prepare('INSERT INTO commentaires(id_billet, auteur, commentaire, date_commentaire, signalements) VALUES(?, ?, ?, NOW(), 0)');
+        $affectedLines = $comments->execute(array($postId, $author, $comment));
+        return $affectedLines;
+    }
+
+    // QUAND LE VISITEUR SIGNALE UN COMMENTAIRE
+    public function reportedCom($id){
+        $bdd = $this->getBdd();
+        $reponse = $bdd->prepare('UPDATE commentaires SET signalements = 1 WHERE id = ?');
+        $reponse->execute(array($id));
+        return $reponse;
+    }
+
+    // AFFICHER LES COMMENTAIRES SIGNALES
+    public function getAllReportedComments(){
+        $bdd = $this->getBdd();
+        $reponse = $bdd->prepare('SELECT * FROM commentaires WHERE signalements > 0');
+        $allReportedComments = $reponse->execute();
+        return $reponse;
+    }
+
+    // SUPPRIMER UN COMMENTAIRE SIGNALE
+    public function adminSuppCom($id){
+        $bdd = $this->getBdd();
+        $reponse = $bdd->prepare('DELETE FROM commentaires WHERE id = ?');
+        $reponse->execute(array($id));
+        $resultat = $reponse->fetch();
+        return $resultat;
+    }
+    // REMETTRE LE COMMENTAIRE SIGNALE
+    public function adminLetCom($id){
+        $bdd = $this->getBdd();
+        $reponse = $bdd->prepare('UPDATE commentaires SET signalements = 0 WHERE id = ?');
+        $reponse->execute(array($id));
+        return $reponse;
+    }
+}
---
+++ modele/PostManager.php
@@ -0,0 +1,68 @@
+<?php
+require_once("Manager.php");
+class PostManager extends Manager
+{
+    //Fonction qui renvoie la liste des 3 derniers articles:
+    public function getLastPosts()
+    {
+        $bdd = $this->getBdd();
+        $reponse = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM articles ORDER BY creation_date DESC LIMIT 0, 3');
+        return $reponse;
+    }
+
+    public function getPost($postId)//récupère un post précis en fonction de son ID
+    {
+        $bdd = $this->getBdd();
+        $reponse = $bdd->prepare('SELECT id, titre, contenu, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM articles WHERE id = ?');
+        $reponse->execute(array($postId));
+        $post = $reponse->fetch();
+        return $post;
+    }
+
+    public function getAllPosts()
+    {
+        $bdd = $this->getBdd();
+        $reponse = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM articles ORDER BY creation_date');
+        return $reponse;
+    }
+
+    //  Récupération de l'utilisateur et de son pass haché
+    public function recover($pseudo){
+        $bdd = $this->getBdd();
+        $reponse = $bdd->prepare('SELECT pseudo, password FROM utilisateur WHERE pseudo = ?');
+        $reponse->execute(array($pseudo));
+        $resultat = $reponse->fetch();
+        return $resultat;
+    }
+    //POUR CREER UN ARTICLE
+    public function adminAddChap($title, $content){
+        $bdd = $this->getBdd();
+        $reponse = $bdd->prepare('INSERT INTO articles(titre, contenu, creation_date) VALUES(?, ?, NOW())');
+        $addChap = $reponse->execute(array($title, $content));
+        return $addChap;
+    }
+    // SUPPRIMER UN ARTICLE
+    public function adminSuppChap($id){
+        $bdd = $this->getBdd();
+        $reponse = $bdd->prepare('DELETE FROM articles WHERE id = ?');
+        $reponse->execute(array($id));
+        $resultat = $reponse->fetch();
+        return $resultat;
+    }
+    // MODIFIER UN CHAPITRE (2 fonctions car 2 boutons: un bouton 'Modifier' dans la page allPostsAdmin 
+    // et un bouton 'Mettre à jour' dans page modify_post)
+    public function modifAdminChap($id){
+        $bdd = $this->getBdd();
+        $reponse = $bdd->prepare('SELECT * FROM articles WHERE id = ?');
+        $modifChap = $reponse->execute(array($id));
+        $result = $reponse->fetch();
+        return $result;
+    }
+
+    public function updateAdminChap($id){
+        $bdd = $this->getBdd();
+        $reponse = $bdd->prepare('UPDATE articles SET titre = ?, contenu = ? WHERE id = ?');
+        $reponse->execute(array($_POST['title'], $_POST['content'], $id));
+        return $reponse;
+    }
+}
